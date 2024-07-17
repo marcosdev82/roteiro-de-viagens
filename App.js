fdb0058fc9e5abca4216676a5c420023cc80463c
mikek182 <marcosvinicius@geekwork.com.br>
2024-07-17 11:25:54 -0300

    Criação de useState para carregamento da pesquisa

--- App.js
+++ App.js
@@ -1,12 +1,16 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View, TextInput, Platform, Pressable } from 'react-native';
+import { useState } from 'react';
+import { StyleSheet, Text, View, TextInput, Platform, Pressable, ScrollView } from 'react-native';
 import { MaterialIcons } from '@expo/vector-icons';
 import Slider from '@react-native-community/slider';
 
 const statusBarHeight = StatusBar.StatusBarHeight
 
 export default function App() {
+
+  const [loading, setLoading] = useState(true);
+  const [travel, setTravel] = useState("");
+
   return (
     <View style={styles.container}>
       <StatusBar barStyle="dark-content" translucent={true} backgroundColor='#f1f1f1'/>
@@ -31,6 +35,17 @@ export default function App() {
         <Text style={styles.buttonText}>Gerar roteiro</Text>
         <MaterialIcons name="travel-explore" size={24} color="#fff"/>
       </Pressable>
+
+      <ScrollView contentContainerStyle={{paddingBottom:24, marginTop: 24,}} style={styles.containerScroll} showsVerticalScrollIndicator>  
+       {travel && (
+        <View style={styles.content}>
+          <Text style={styles.title}>Roteiro da viagem</Text>
+          <Text>Aqui vai ser o roteiro completo...Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
+          Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
+          </Text>
+        </View>
+       )}
+      </ScrollView>
 
     </View>
   );
@@ -75,6 +90,33 @@ const styles = StyleSheet.create({
   button: {
     backgroundColor: '#FF5656',
     width: '90%',
+    borderRadius: 8,
     padding: 14,
+    gap: 8,
+    flexDirection: 'row',
+    justifyContent: 'center',
+    alignItems: 'center',
   },
+  buttonText: {
+    fontSize: 18,
+    color: '#fff',
+    fontWeight: 'bold',
+  },
+  content: {
+    backgroundColor: '#fff',
+    padding:16,
+    width: '100%',
+    marginTop: 16,
+    borderRadius: 8,
+  },
+  title: {
+    fontSize: 18,
+    fontWeight: 'bold',
+    alignItems: 'center',
+    marginBottom: 14,
+  },
+  containerScroll: {
+    width: '90%',
+    marginTop: 8,
+  } 
 });
